277c59a606ec3cb0a45cad6f8da58f79bc20fe77
Isam6 <issamdiez10@gmail.com>
2025-05-15 17:04:33 +0200

    Add Disk Entity class

---
+++ cpd-project-app/api-service/src/main/java/es/urjc/api_service/Model/Disk.java
@@ -0,0 +1,69 @@
+package es.urjc.api_service.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Disk {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int size;
+
+    @Enumerated(EnumType.STRING)
+    private DiskType type;
+
+    @Enumerated(EnumType.STRING)
+    private DiskStatus status;
+
+    public enum DiskType { HDD, SSD }
+    public enum DiskStatus { REQUESTED, INITIALIZED, ASSIGNED, UNASSIGNED }
+
+    //Constructor
+    public Disk() {}
+
+    public Disk(int size, DiskType type, DiskStatus status) {
+        this.size = size;
+        this.type = type;
+        this.status = status;
+    }
+
+    //Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public DiskType getType() {
+        return type;
+    }
+
+    public void setType(DiskType type) {
+        this.type = type;
+    }
+
+    public DiskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DiskStatus status) {
+        this.status = status;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Disk{" + "id=" + id + ", size=" + size + ", type=" + type + ", status=" + status + "}";
+    }
+
+}
