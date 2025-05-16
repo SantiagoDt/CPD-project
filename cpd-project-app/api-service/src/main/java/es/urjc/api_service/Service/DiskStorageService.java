4a85b0b57e6436b15da970329f17a3e5caa706a5
Isam6 <issamdiez10@gmail.com>
2025-05-16 10:15:47 +0200

    Add DiskStorageService with paginated findAll and status filter

---
+++ cpd-project-app/api-service/src/main/java/es/urjc/api_service/Service/DiskStorageService.java
@@ -0,0 +1,36 @@
+package es.urjc.api_service.Service;
+
+import es.urjc.api_service.Model.Disk;
+import es.urjc.api_service.Repository.DiskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DiskStorageService {
+    @Autowired
+    private DiskRepository diskRepository;
+
+    public Page<Disk> findAll(Pageable pageable) {
+        return diskRepository.findAll(pageable);
+    }
+
+    public Optional<Disk> findById(Long id) {
+        return diskRepository.findById(id);
+    }
+
+    public Disk save(Disk disk) {
+        return diskRepository.save(disk);
+    }
+
+    public void deleteById(Long id) {
+        diskRepository.deleteById(id);
+    }
+
+    public Optional<Disk> findFirstByStatus(Disk.DiskStatus status) {
+        return diskRepository.findFirstByStatus(status);
+    }
+}
