13d56e94835fb127e49a55e2e2399858961d35de
AlexanderMenMen <83810151+AlexanderMenMen@users.noreply.github.com>
2025-05-16 10:27:42 +0200

    Add InstanceStorageService with paginated findAll

---
+++ cpd-project-app/api-service/src/main/java/es/urjc/api_service/Service/InstanceStorageService.java
@@ -0,0 +1,56 @@
+package es.urjc.api_service.Service;
+
+import es.urjc.api_service.Model.Disk;
+import es.urjc.api_service.Model.Instance;
+import es.urjc.api_service.Repository.DiskRepository;
+import es.urjc.api_service.Repository.InstanceRepositoy;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class InstanceStorageService {
+    @Autowired
+    private InstanceRepositoy instanceRepositoy;
+
+    @Autowired
+    private DiskRepository diskRepository;
+
+    public Page<Instance> findAll(Pageable pageable){
+        return instanceRepositoy.findAll(pageable);
+    }
+
+    public Optional<Instance> findById(Long id){
+        return instanceRepositoy.findById(id);
+    }
+
+    public Instance save(Instance instance){
+        return instanceRepositoy.save(instance);
+    }
+
+    public void deleteById(Long id) {
+        instanceRepositoy.deleteById(id);
+    }
+
+    @PostConstruct
+    public void init() {
+        Disk disk1 = new Disk(100, Disk.DiskType.SSD, Disk.DiskStatus.ASSIGNED);
+        Disk disk2 = new Disk(250, Disk.DiskType.HDD, Disk.DiskStatus.INITIALIZED);
+        Disk disk3 = new Disk(500, Disk.DiskType.SSD, Disk.DiskStatus.ASSIGNED);
+        diskRepository.save(disk1);
+        diskRepository.save(disk2);
+        diskRepository.save(disk3);
+
+        Instance instance1 = new Instance("server", 4, 2, "192.0.0.1", Instance.InstanceStatus.ASSIGNING_IP, disk1);
+        Instance instance2 = new Instance("webapp", 8, 4, "192.0.0.2", Instance.InstanceStatus.RUNNING, disk2);
+        Instance instance3 = new Instance("database", 16, 8, "192.0.0.3", Instance.InstanceStatus.STARTING, disk3);
+        instanceRepositoy.save(instance1);
+        instanceRepositoy.save(instance2);
+        instanceRepositoy.save(instance3);
+    }
+}
