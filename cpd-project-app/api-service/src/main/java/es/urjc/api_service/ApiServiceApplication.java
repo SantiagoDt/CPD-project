2dab4ba36d7fc8a90c95f84394f9bcbfde9b4dfb
SantiagoDt <98756849+SantiagoDt@users.noreply.github.com>
2025-05-23 16:20:37 +0200

    Test message sending from application startup and config updates

--- cpd-project-app/api-service/src/main/java/es/urjc/api_service/ApiServiceApplication.java
+++ cpd-project-app/api-service/src/main/java/es/urjc/api_service/ApiServiceApplication.java
@@ -1,7 +1,11 @@
 package es.urjc.api_service;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiServiceApplication {
@@ -10,4 +14,15 @@ public class ApiServiceApplication {
 		SpringApplication.run(ApiServiceApplication.class, args);
 	}
 
+	@Bean
+	public Queue myQueue() {
+		return new Queue("disk-requests", false);
+	}
+
+	@Bean
+	public MessageConverter messageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
 }
+
