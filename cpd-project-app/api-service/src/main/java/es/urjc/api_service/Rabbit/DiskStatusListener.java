2b851a227b37230c839c1a1903131857124517ff
CristianInBits <128726794+CristianInBits@users.noreply.github.com>
2025-05-24 22:31:08 +0200

    Add DiskStatusListener to handle disk-related status events

---
+++ cpd-project-app/api-service/src/main/java/es/urjc/api_service/Rabbit/DiskStatusListener.java
@@ -0,0 +1,43 @@
+package es.urjc.api_service.Rabbit;
+
+import es.urjc.api_service.Dto.DiskStatusDTO;
+import es.urjc.api_service.Dto.InstanceRequestDTO;
+import es.urjc.api_service.Model.Disk;
+import es.urjc.api_service.Model.Instance;
+import es.urjc.api_service.Service.DiskStorageService;
+import es.urjc.api_service.Service.MessageProducerService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DiskStatusListener {
+
+    @Autowired
+    DiskStorageService diskService;
+    @Autowired
+    MessageProducerService messageService;
+
+    @RabbitListener(queues = "disk-statuses", ackMode = "AUTO")
+    public void process(DiskStatusDTO statusDTO) {
+
+        Optional<Disk> diskOpt = diskService.findById(statusDTO.getDiskId());
+        if (diskOpt.isPresent()) {
+            Disk disk = diskOpt.get();
+            disk.setStatus(statusDTO.getStatus());
+            diskService.save(disk);
+            if (disk.getStatus() == Disk.DiskStatus.ASSIGNED) {
+                Instance instance = disk.getInstance();
+                InstanceRequestDTO instanceRequestDTO = new InstanceRequestDTO(instance.getId(), instance.getName(),
+                        instance.getCores(), instance.getMemory());
+                messageService.sendInstanceRequest(instanceRequestDTO);
+            }
+        } else {
+            System.err.println("No se encontró el disco con ID: " + statusDTO.getDiskId());
+        }
+
+    }
+
+}
