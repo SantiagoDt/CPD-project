25f490f2c3a425aaebede9308efb4cdc336fecc8
Isam6 <issamdiez10@gmail.com>
2025-05-25 11:22:26 +0200

    Fixing entities generated values mode + instanceStorageService post construct + application preperties

--- cpd-project-app/api-service/src/main/java/es/urjc/api_service/Service/InstanceStorageService.java
+++ cpd-project-app/api-service/src/main/java/es/urjc/api_service/Service/InstanceStorageService.java
@@ -46,33 +46,4 @@ public class InstanceStorageService {
         return instance;
     }
 
-    @PostConstruct
-    public void init() {
-        Disk disk1 = new Disk(100, Disk.DiskType.SSD, Disk.DiskStatus.ASSIGNED);
-        Disk disk2 = new Disk(250, Disk.DiskType.HDD, Disk.DiskStatus.ASSIGNED);
-        Disk disk3 = new Disk(500, Disk.DiskType.SSD, Disk.DiskStatus.ASSIGNED);
-
-        disk1 = diskRepository.save(disk1);
-        disk2 = diskRepository.save(disk2);
-        disk3 = diskRepository.save(disk3);
-
-        Instance instance1 = new Instance("IronFortress", 4, 2, "192.168.1.2", Instance.InstanceStatus.ASSIGNING_IP,
-                disk1);
-        Instance instance2 = new Instance("NimbusNode", 8, 4, "192.168.1.3", Instance.InstanceStatus.ASSIGNING_IP,
-                disk2);
-        Instance instance3 = new Instance("DataKraken", 16, 8, "192.168.1.4", Instance.InstanceStatus.ASSIGNING_IP,
-                disk3);
-
-        disk1.setInstance(instance1);
-        disk2.setInstance(instance2);
-        disk3.setInstance(instance3);
-
-        instanceRepositoy.save(instance1);
-        instanceRepositoy.save(instance2);
-        instanceRepositoy.save(instance3);
-
-        diskRepository.save(disk1);
-        diskRepository.save(disk2);
-        diskRepository.save(disk3);
-    }
 }
