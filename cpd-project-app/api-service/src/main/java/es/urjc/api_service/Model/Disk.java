82293ea9c85904d2f4dbf8cdd1974437092b0825
Isam6 <issamdiez10@gmail.com>
2025-05-25 11:22:26 +0200

    Fixing entities generated values mode + instanceStorageService post construct + application preperties

--- cpd-project-app/api-service/src/main/java/es/urjc/api_service/Model/Disk.java
+++ cpd-project-app/api-service/src/main/java/es/urjc/api_service/Model/Disk.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 @Entity
 public class Disk {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int size;
 
